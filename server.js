14d7102f28af9428007aebc7891109c564964b30
Sophie <sophbyr@gmail.com>
2021-02-09 13:58:27 +0100

    [+] Change cors

--- server.js
+++ server.js
@@ -28,7 +28,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: false
 }));
-app.use(cors()); 
+var allowedOrigins = ['http://localhost:4000',
+                      'https://dino-friends.netlify.app/'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'dist/')));
 app.use('/', express.static(path.join(__dirname, 'dist/')));
